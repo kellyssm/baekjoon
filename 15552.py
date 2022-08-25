185799f3ffda07683dbf9a87ecdf91ce2c613c75
Soomin <83341338+kellyssm@users.noreply.github.com>
2022-08-25 15:12:49 +0900

    Update 15552.py

--- 15552.py
+++ 15552.py
@@ -19,3 +19,6 @@ for i in range(num):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다. 
 # BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+
+
+# BOJ라고 부르나봄.

1b98ca6c7a49a2bfc060783b01c348a62a08e331
Soomin <83341338+kellyssm@users.noreply.github.com>
2022-08-25 09:42:52 +0900

    Update baekjoon_2480.py

--- baekjoon_2480.py
+++ baekjoon_2480.py
@@ -20,11 +20,15 @@ for i in range(3):
 #위에 내가 한건데 숫자 비교할 필요 없었긔
 ```````````````````````````````````````
 
-# if a==b==c:
-#     print(10000+(a*1000))
-# elif a==b!=c:
-#     print(1000+(a*100))
-# elif a!=b==c:
-#     print(1000+(b*100))
+# a, b, c = map(int, input().split())
+
+# if a == b == c:
+#     print(10000+a*1000)
+# elif a == b:
+#     print(1000+a*100)
+# elif a == c:
+#     print(1000+a*100)
+# elif b == c:
+#     print(1000+b*100)
 # else:
-#     print(a*100)
+#     print(100 * max(a,b,c))

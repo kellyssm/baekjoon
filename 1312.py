0e8c7b884922c222f34b7c47457fe890d7a82a24
Sumin <83341338+kellyssm@users.noreply.github.com>
2024-05-07 16:17:50 +0900

    소수

--- 1312.py
+++ 1312.py
@@ -5,4 +5,14 @@ sumt = n[0] / n[1]
 result = str(sum).split('.')[1]
 print(int(result[k]))
 
-#왜자꾸 런타임 에러가 나는거임?
+#왜자꾸 런타임 에러가 나는거임? => n범위 지정 안해줘서
+#혜린이 코드
+'''
+A, B, N = map(int, input().split())
+
+for i in range(N):
+    A = (A%B)*10
+    res = A//B
+
+print(res)
+'''

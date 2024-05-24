512db741c0c424195923c535f2b0e2adef4a9024
Sumin <83341338+kellyssm@users.noreply.github.com>
2024-05-24 13:20:35 +0900

    분수합

--- 1735.py
+++ 1735.py
@@ -1,8 +1,20 @@
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+from math import gcd
 
-result=list()
-result.append(a[0]*b[1]+b[0])
-result.apeend(a[1]*b[1])
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+temp = []
 
-if result[0]%result[1]==1: #기약분수로 만드는거.
+temp.append(a[0] * b[1] + b[0] * a[1])
+temp.append(a[1] * b[1])
+
+numerator = temp[0] #분자
+denominator = temp[1] #분모
+gcdn = gcd(numerator, denominator) #기약분수 만들기
+
+
+simplified_numerator = numerator // gcdn
+simplified_denominator = denominator // gcdn
+
+print(simplified_numerator, simplified_denominator)
+
+
